9e9083740227c3e9ebb8bedc5a7bb6950e25b5d6
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-31 10:36:30 +0200

    Add ”for parents” post type

--- library/App.php
+++ library/App.php
@@ -5,6 +5,10 @@ class App
 {
     public function __construct()
     {
+        // Theme
         new \MunicipioSchool\Theme\Enqueue();
+
+        // Post types
+        new \MunicipioSchool\PostTypes\ForParents();
     }
 }

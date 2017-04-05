61dd7b283e4e1af9a5feef834c1bfa730cc18074
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-04-05 10:59:58 +0200

    Fix namespace caps

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
 }
 
 require_once MUNICIPIOSCHOOL_PATH . 'library/Vendor/Psr4ClassLoader.php';
-$loader = new MUNICIPIOSCHOOL\Vendor\Psr4ClassLoader();
+$loader = new MunicipioSchool\Vendor\Psr4ClassLoader();
 $loader->addPrefix('MunicipioSchool', MUNICIPIOSCHOOL_PATH . 'library');
 $loader->addPrefix('MunicipioSchool', MUNICIPIOSCHOOL_PATH . 'source/php/');
 $loader->register();
